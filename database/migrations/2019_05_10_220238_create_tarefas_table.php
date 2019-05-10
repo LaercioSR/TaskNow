5165d1912a84c596928a0921eb82e555588b2e0a
LaercioSR <laercio.rios13@hotmail.com>
2019-05-10 19:10:24 -0300

    Adicionado migrações tipo_tarefa e tarefa

---
+++ database/migrations/2019_05_10_220238_create_tarefas_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTarefasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tarefas', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('id_usuario');
+            $table->unsignedBigInteger('id_tipo');
+            $table->string('titulo');
+            $table->boolean('privacidade');
+            $table->string('descricao');
+            $table->boolean('status');
+            $table->date('data_conclusao');
+            $table->foreign('id_tipo')->references('id')->on('tipo_tarefas');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tarefas');
+    }
+}
